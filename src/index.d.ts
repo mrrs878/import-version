5142e09686a8746a5de9d160972e7624d3601154
mrrs878 <mrrs878@foxmail.com>
2022-02-14 22:43:27 +0800

    lint: 添加缺少的类型声明

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: mrrs878@foxmail.com
+ * @Date: 2022-02-14 22:35:33
+ * @LastEditors: mrrs878@foxmail.com
+ * @LastEditTime: 2022-02-14 22:35:33
+ */
+
+declare namespace NodeJS {
+  interface Process {
+    binding: any;
+  }
+}
